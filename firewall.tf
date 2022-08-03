a3670ec5917a241894b3f93353e23bd528ebe64f
KoLiBer <koliberr136a1@gmail.com>
2022-08-03 12:03:15 +0430

    fix: add description to firewall rule

--- firewall.tf
+++ firewall.tf
@@ -2,9 +2,10 @@ resource "hcloud_firewall" "this" {
   name = var.name
 
   rule {
-    direction  = "in"
-    protocol   = "udp"
-    source_ips = ["0.0.0.0/0", "::/0"]
-    port       = "51820"
+    port            = "51820"
+    protocol        = "udp"
+    direction       = "in"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+    description     = "Wireguard Traffic"
   }
 }
