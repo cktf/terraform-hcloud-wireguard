8d4d58bf76a4f70f33cdc54224f1a50fbaa13a07
KoLiBer <koliberr136a1@gmail.com>
2022-08-03 12:05:13 +0430

    fix: add egress rules to firewall

--- firewall.tf
+++ firewall.tf
@@ -8,4 +8,24 @@ resource "hcloud_firewall" "this" {
     destination_ips = ["0.0.0.0/0", "::/0"]
     description     = "Wireguard Traffic"
   }
+  rule {
+    protocol        = "icmp"
+    direction       = "out"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+    description     = "ICMP Internet Traffic"
+  }
+  rule {
+    port            = "any"
+    protocol        = "tcp"
+    direction       = "out"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+    description     = "TCP Internet Traffic"
+  }
+  rule {
+    port            = "any"
+    protocol        = "udp"
+    direction       = "out"
+    destination_ips = ["0.0.0.0/0", "::/0"]
+    description     = "UDP Internet Traffic"
+  }
 }
