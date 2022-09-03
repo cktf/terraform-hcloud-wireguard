0c7e416b7735e7711712c2686ad88a50fd974c48
KoLiBer <koliberr136a1@gmail.com>
2022-09-03 11:10:28 +0430

    fix: change firewall ingress rule

--- firewall.tf
+++ firewall.tf
@@ -2,11 +2,11 @@ resource "hcloud_firewall" "this" {
   name = var.name
 
   rule {
-    port            = "51820"
-    protocol        = "udp"
-    direction       = "in"
-    destination_ips = ["0.0.0.0/0", "::/0"]
-    description     = "Wireguard Traffic"
+    port        = "51820"
+    protocol    = "udp"
+    direction   = "in"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+    description = "Wireguard Traffic"
   }
   rule {
     protocol        = "icmp"
